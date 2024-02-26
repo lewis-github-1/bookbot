d85429ab782ac0289e42748e04b09def4890103f
lewis-github-1 <ltowne2@gmail.com>
2024-02-25 21:14:54 -0600

    step 16 done

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ def main():
         file_contents = f.read()        # stored frankenstein text
         words = file_contents.split()   # frankenstein text split into words
         words_count = len(words)        # word count
-        print(words_count)
-
-        count_char(file_contents)
-
-
+        
+        letter_counts = count_char(file_contents)
+        agg = aggregate(letter_counts)
+        
+        print("--- Begin report of books/frankenstein.txt ---")
+        print(f"{words_count} words found in the document")
+        for item in agg:
+            print(f"The {item['name']} character was found {item['num']} times")
+        print("--- End report ---")
 
 
 def count_char(file_contents):
@@ -19,9 +23,21 @@ def count_char(file_contents):
                 letter_count[i] = 1
             else:
                 letter_count[i] += 1
+    return letter_count
     
-    print(letter_count)
+def aggregate(letter_counts):    
+    dict_list = []
+    for key, value in letter_counts.items():
+        dict_list.append({"name": key, "num": value})
     
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
+
+def sort_on(dict):
+    return dict["num"]
+
 
 
 main()
+
+
